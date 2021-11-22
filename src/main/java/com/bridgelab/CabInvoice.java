9bf77f08ba302524d6f409edf82af12b75777bae
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-22 17:18:53 +0530

    Welcome To Cab_invoice_generator problem

---
+++ src/main/java/com/bridgelab/CabInvoice.java
@@ -0,0 +1,11 @@
+package com.bridgelab;
+
+public class CabInvoice {
+    private static final int COST_PER_KM=10;
+    private static final int COST_PER_MINUTE=1;
+    private static final int MINIMUM_FARE=5;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator problem");
+    }
+}
